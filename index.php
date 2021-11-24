6d4daac0fa984beca1768a1cadea8e7e57d31431
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2021-11-24 19:22:24 +0700

    add php file

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+error_reporting(E_ALL);
+session_start();
+
+$page = array(
+	'login' => 'file:///F:/Project/Web/mogreen/login.html',
+	'home' => 'file:///F:/Project/Web/mogreen/home.html'
+);
+
+$code = array('key' => 'cvmogreen18', 'hash' => password_hash('cvmogreen18', PASSWORD_DEFAULT));
+
+if( isset($_POST['code']) ){
+	if( password_verify($_POST['code'], $code['hash']) ){
+		$_SESSION['login'] = true;
+
+		if( isset($_POST['rememberMe']) ){
+			setcookie('mgr_code', $code['hash'], time() + (3600 * 24));
+		}
+	}
+}
+
+if( isset($_COOKIE['mgr_code']) ){
+	if( password_verify($code['key'], $_COOKIE['mgr_code']) ){
+		$_SESSION['login'] = true;
+	}
+}
+
+$login = false;
+if( isset($_SESSION('login')) ){
+	if($_SESSION['login']){ $login = true; }
+}
+
+if( isset($_SESSION('login')) ){ require $page['home']; }else{ require $page['login']; }
