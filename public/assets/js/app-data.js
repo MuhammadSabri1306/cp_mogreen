7e595f77c873ba7b6f5ee9f27623cac07a08a949
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2021-12-23 19:15:30 +0700

    unimplementation of vue.js

---
+++ public/assets/js/app-data.js
@@ -0,0 +1,34 @@
+var appData = {
+	img: {
+		header: {
+			lg: "file:///F:/download/mogreen/20211218_120456.jpg",
+			sm: "file:///F:/download/mogreen/20211218_120456.jpg"
+		},
+		gallery: [
+			{
+				lg: "file:///F:/download/mogreen/20211218_120233.jpg",
+				sm: "file:///F:/download/mogreen/20211218_120233.jpg"
+			}, {
+				lg: "file:///F:/download/mogreen/20211218_133047.jpg",
+				sm: "file:///F:/download/mogreen/20211218_133047.jpg"
+			}, {
+				lg: "file:///F:/download/mogreen/20211218_133047.jpg",
+				sm: "file:///F:/download/mogreen/20211218_133047.jpg"
+			}
+		],
+	},
+	products: {
+		carrot: {
+			name: "Carrot",
+			description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			img: "F:/Project/Web/mogreen/public/products/produk_wortel.jpg",
+			imgSM: "F:/Project/Web/mogreen/public/products/produk_wortel-sm.jpg"
+		},
+		potato: {
+			name: "Potato",
+			description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+			img: "F:/Project/Web/mogreen/public/products/produk_kentang.jpg",
+			imgSM: "F:/Project/Web/mogreen/public/products/produk_kentang-sm.jpg"
+		}
+	}
+};
